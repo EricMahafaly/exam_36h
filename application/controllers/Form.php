ad25b6b896a425d99f63a54cbadd619ad579d1db
EricMahafaly <Ericmahafaly7@gmail.com>
2023-07-09 18:42:56 +0200

    add files

---
+++ application/controllers/Form.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Form extends CI_Controller {
+    // affiche view Login
+    public function loginView() {
+        $this->load->view('/formulaire/login');
+    }
+
+    // controller login
+    public function loginController() {
+        $this->load->library('session');
+        $mdp = $this->input->post('mdp');
+        if($mdp == 'eric') {
+            // Définir une valeur de session
+            $this->session->set_userdata('pass', $mdp);
+            
+            // Récupérer une valeur de session
+            $username = $this->session->userdata('pass');
+
+            $this->load->view('/pages/accueil');
+        } else {
+            echo "echoue";
+        }
+    }
+
+}
---
+++ application/views/formulaire/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap-3.3.6-dist/css/bootstrap.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/login.css"); ?>">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="col-md-12 title">
+                    Sign In
+                </div>
+                <form action="<?php echo site_url("Form/loginController"); ?>" method="post">
+                    <p>
+                        <input type="email" name="email" placeholder="email" required>
+                    </p>
+                    <p>
+                        <input type="text" name="mdp" placeholder="mots de passe" required>
+                    </p>
+                    <p>
+                        <input type="submit" class="btn" value="Valider">
+                    </p>
+                </form>
+                <div>
+                    Don't have an account?
+                    <br/>
+                    <span>SIGN UP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
