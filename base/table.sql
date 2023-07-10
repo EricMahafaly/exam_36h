ae2157e5d5d2c3781ab5ebd2cc977ffe6149810d
EricMahafaly <Ericmahafaly7@gmail.com>
2023-07-10 19:54:41 +0200

    Update table.sql

--- base/table.sql
+++ base/table.sql
@@ -35,11 +35,8 @@ CREATE TABLE user_code(
 );
 
 CREATE TABLE sport(
-    id SERIAL PRIMARY KEY ,
-    nom VARCHAR (50) ,
-    description VARCHAR(255) ,
-    frequence INTEGER ,
-    repos DOUBLE PRECISION
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR (50),
 );
 
 CREATE TABLE nutrition(
@@ -48,33 +45,10 @@ CREATE TABLE nutrition(
     categorie VARCHAR(50) 
 );
 
-CREATE TABLE gagner_poids(
-    id SERIAL PRIMARY KEY ,
-    couple INTEGER ,
-    eau_par_jour DOUBLE PRECISION ,
-    poids_min INTEGER ,
-    poids_max INTEGER ,
-    genre INTEGER ,
-    duree_sport DOUBLE PRECISION ,
-    poids_nutrition DOUBLE PRECISION ,
-    sport_id INTEGER REFERENCES sport ,
-    nutrition_id INTEGER REFERENCES nutrition 
-);
+CREATE TABLE regime (
+    id_regime SERIAL PRIMARY KEY,
 
-CREATE TABLE perdre_poids(
-    id SERIAL PRIMARY KEY ,
-    couple INTEGER ,
-    eau_par_jour DOUBLE PRECISION ,
-    poids_min INTEGER ,
-    poids_max INTEGER ,
-    genre INTEGER ,
-    duree_sport DOUBLE PRECISION ,
-    poids_nutrition DOUBLE PRECISION ,
-    sport_id INTEGER REFERENCES sport ,
-    nutrition_id INTEGER REFERENCES nutrition 
-);
+)
 
-CREATE TABLE IMC(
-    id SERIAL PRIMARY KEY,
-    resultat DOUBLE PRECISION
-);
+
+
